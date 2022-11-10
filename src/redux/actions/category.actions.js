f09242c1bbf83289506bd22f3eb398b3af57a5c5
Katy Tran <74617368+katytran@users.noreply.github.com>
2022-11-09 18:01:47 -0800

    Update category.actions.js

--- src/redux/actions/category.actions.js
+++ src/redux/actions/category.actions.js
@@ -14,32 +14,6 @@ const getAllCategory = () => async (dispatch) => {
   }
 };
 
-// const getAllCategory = (pageNum, limit, query, sortBy) => async (dispatch) => {
-//   dispatch({ type: types.GET_ALL_CATEGORY_REQUEST });
-//   try {
-//     pageNum = pageNum || 1;
-//     limit = limit || 100;
-//     let queryString = "";
-//     if (query) {
-//       //queryString = `&name[$regex]=${query}&name[$options]=i&brand[$regex]=${query}&brand[$options]=i`;
-//       queryString = query;
-//     }
-
-//     let sortByString = "";
-//     if (sortBy?.key) {
-//       sortByString = `&sortBy[${sortBy.key}]=${sortBy.ascending}`;
-//     }
-//     const res = await api.get(
-//       `/categorys?page=${pageNum}&limit=${limit}&query=${queryString}${sortByString}`
-//     );
-//     dispatch({
-//       type: types.GET_ALL_CATEGORY_SUCCESS,
-//       payload: res.data.data,
-//     });
-//   } catch (error) {
-//     dispatch({ type: types.GET_ALL_CATEGORY_FAILURE, payload: error });
-//   }
-// };
 
 const categoryActions = {
   getAllCategory,
